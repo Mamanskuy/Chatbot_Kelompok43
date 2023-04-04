243455f18fe669dd736c0836e6a8f35b5d4f9b4e
kepitingcungkring <129749396+kepitingcungkring@users.noreply.github.com>
2023-04-04 11:41:43 +0700

    Update Chatbot.py

--- Chatbot.py
+++ Chatbot.py
@@ -1,37 +1,41 @@
-def generate_response(user_input):
-    sapaan = ["hai", "halo", "hi", "hello", "selamat pagi", "selamat siang", "selamat sore", "selamat malam"]
-    Exit = ["byee", "goodbye", "sampai jumpa", "dada", "see you", "sampai nanti"]
-    Trims = ["terima kasih", "thanks", "makasih", "sukron", "terima kasih", "Suwun"]
-    if user_input.lower() in sapaan:
-        responses = ["Hai! Ada yang bisa saya bantu?", "Selamat datang! Ada yang bisa saya bantu?"]
-        return random.choice(responses)
-    elif user_input.lower() in Exit:
-        responses = ["Terima kasih telah menggunakan chatbot ini. Sampai jumpa!", "Sampai jumpa lagi!"]
-        return random.choice(responses)
-    elif user_input.lower() in Trims:
-        responses = ["Sama-sama! Ada lagi yang bisa saya bantu?", "Tidak masalah. Ada lagi yang bisa saya bantu?"]
-        return random.choice(responses)
+import random
+
+class Chatbot:
+    def _init_(self):
+        self.sapaan = ["hai", "halo", "hi", "hello", "selamat pagi", "selamat siang", "selamat sore", "selamat malam"]
+        self.Exit = ["byee", "goodbye", "sampai jumpa", "dada", "see you", "sampai nanti"]
+        self.Trims = ["terima kasih", "thanks", "makasih", "sukron", "terima kasih", "Suwun"]
     
-    else:
-        responses = [
-            "Maaf, saya tidak mengerti. Bisa dijelaskan lagi?",
-            "Hmm, saya kurang paham. Ada yang bisa saya bantu?",
-            "Silakan sampaikan masalah Anda."
-        ]
-        return random.choice(responses)
-
-def chat():
-    print("Halo! Selamat datang di chatbot sederhana ini.")
-    print("Silakan sampaikan pertanyaan atau masalah Anda.")
-    while True:
-        user_input = input("Anda: ")
-        if user_input.lower() in ["keluar", "exit"]:
-            print("Chatbot: " + generate_response(user_input))
-            break
-        elif user_input == "":
-            continue
+    def generate_response(self, user_input):
+        if user_input.lower() in self.sapaan:
+            responses = ["Hai! Ada yang bisa saya bantu?", "Selamat datang! Ada yang bisa saya bantu?"]
+            return random.choice(responses)
+        elif user_input.lower() in self.Exit:
+            responses = ["Terima kasih telah menggunakan chatbot ini. Sampai jumpa!", "Sampai jumpa lagi!"]
+            return random.choice(responses)
+        elif user_input.lower() in self.Trims:
+            responses = ["Sama-sama! Ada lagi yang bisa saya bantu?", "Tidak masalah. Ada lagi yang bisa saya bantu?"]
+            return random.choice(responses)
         else:
-            print("Chatbot: " + generate_response(user_input))
+            responses = [
+                "Maaf, saya tidak mengerti. Bisa dijelaskan lagi?",
+                "Hmm, saya kurang paham. Ada yang bisa saya bantu?",
+                "Silakan sampaikan masalah Anda."
+            ]
+            return random.choice(responses)
 
+    def start_chat(self):
+        print("Halo! Selamat datang di chatbot sederhana ini.")
+        print("Silakan sampaikan pertanyaan atau masalah Anda.")
+        while True:
+            user_input = input("Anda: ")
+            if user_input.lower() in self.Exit:
+                print("Chatbot: " + self.generate_response(user_input))
+                break
+            elif user_input == "":
+                continue
+            else:
+                print("Chatbot: " + self.generate_response(user_input))
 
-chat()
+chatbot = Chatbot()
+chatbot.start_chat()
